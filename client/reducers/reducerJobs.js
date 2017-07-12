f4265c4ae22e3b382f5d71bbd8faa0dfba56d5f4
codypearce <codyapearce@gmail.com>
2017-07-11 18:16:06 -0600

    set up jobs reducer with demo data

---
+++ client/reducers/reducerJobs.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Junior Developer', company: 'Google', salary: '80,000', location: 'Mountain View, CA', },
+    { title: 'Ruby on Rails Dev', company: 'Utah Startup', salary: '75,000', location: 'Logan, UT', },
+    { title: 'Dev Ops Developer', company: 'Digital Ocean', salary: '110,000', location: 'New York, NY', },
+    { title: 'Unity Developer', company: 'Rockstar', salary: '120,000', location: 'New York, NY', },
+  ]
+}
