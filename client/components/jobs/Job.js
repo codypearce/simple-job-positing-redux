ffab25bac5fc1c233fde288d6bd6702d6ce274d7
codypearce <codyapearce@gmail.com>
2017-07-12 19:55:03 -0600

    fix row spacing, click name, fix pointer

--- client/components/jobs/Job.js
+++ client/components/jobs/Job.js
@@ -5,19 +5,21 @@ export default class Job extends Component {
   constructor() {
     super();
   }
-
+  _handleClick() {
+    console.log(this.props.job)
+  }
   render() {
     const { job } = this.props;
     return (
       <div className="row my-4">
         <div className="col-4">
-          <h2>{job.title}</h2>
+          <h2 className="pointer" onClick={() => this._handleClick()}>{job.title}</h2>
           <p>{job.company}</p>
         </div>
         <div className="col">
         <p>{job.salary}</p>
         </div>
-        <div  className="col">
+        <div className="col">
           <p>{job.location}</p>
         </div>
         <div  className="col">
