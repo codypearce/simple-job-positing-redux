02b74cfab422a552c181055ff8efe57f14013a2f
codypearce <codyapearce@gmail.com>
2017-07-13 22:19:43 -0600

    add active job reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import JobsReducer from './reducerJobs';
+import ActiveJob from './reducerActiveJob';
 
 const rootReducer = combineReducers({
   jobs: JobsReducer,
+  activeJob: ActiveJob
 });
 
 export default rootReducer;
