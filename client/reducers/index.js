b5e6449f88ee160f77101492b6e2bec7ea31698d
codypearce <codyapearce@gmail.com>
2017-07-14 23:34:04 -0600

    set up filtered jobs reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import JobsReducer from './reducerJobs';
 import ActiveJob from './reducerActiveJob';
+import FilteredJobs from './filteredJobs';
 
 const rootReducer = combineReducers({
   jobs: JobsReducer,
-  activeJob: ActiveJob
+  activeJob: ActiveJob,
+  filteredJobs: FilteredJobs
 });
 
 export default rootReducer;
