1a3b24611d542716ceb6513b9ddab01d33f5a8f8
codypearce <codyapearce@gmail.com>
2017-07-18 19:29:04 -0600

    style and layout addjob

--- client/containers/AddJob.js
+++ client/containers/AddJob.js
@@ -10,14 +10,17 @@ export default class ActiveJob extends Component {
  
   render() {
     return (
-     	<div>
-     		<h3>Add Job</h3>
-     		<form>
-     			<input type="text" placeholder="Title" />
-     			<input type="text" placeholder="Company" />
-     			<input type="text" placeholder="Location" />
-     			<textarea></textarea>
-     			<input type='submit' className="btn btn-primary" value="Add job" />
+     	<div className="container">
+     		<h3 className="text-center mb-5">Add Job</h3>
+     		<form className="d-flex flex-column ">
+     			<input className="form-control mb-3 w-50 mx-auto" type="text" placeholder="Title" />
+     			<input className="form-control mb-3 w-50 mx-auto" type="text" placeholder="Company" />
+     			<input className="form-control mb-3 w-50 mx-auto" type="text" placeholder="Location" />
+     			<div className="form-group">
+					<label htmlFor="textarea" className="d-block text-center mx-auto">Description</label>
+					<textarea className="form-control mb-3 w-50 mx-auto" id="textarea" rows="3"></textarea>
+				</div>
+     			<input type='submit' className="btn btn-primary w-50 mx-auto" value="Add job" />
      		</form>
     	</div>
     )
