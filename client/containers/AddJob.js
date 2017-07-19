6ea2061e0b6be3d74bc61a1c3e7f19778d733d0e
codypearce <codyapearce@gmail.com>
2017-07-18 19:31:57 -0600

    fix form button, handleform submit

--- client/containers/AddJob.js
+++ client/containers/AddJob.js
@@ -2,27 +2,29 @@ import React, { Component } from 'react';
 
 
 export default class ActiveJob extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
+	_onFormSubmit(event) {
+		event.preventDefault();
+	}
  
- 
-  render() {
-    return (
-     	<div className="container">
-     		<h3 className="text-center mb-5">Add Job</h3>
-     		<form className="d-flex flex-column ">
-     			<input className="form-control mb-3 w-50 mx-auto" type="text" placeholder="Title" />
-     			<input className="form-control mb-3 w-50 mx-auto" type="text" placeholder="Company" />
-     			<input className="form-control mb-3 w-50 mx-auto" type="text" placeholder="Location" />
-     			<div className="form-group">
-					<label htmlFor="textarea" className="d-block text-center mx-auto">Description</label>
-					<textarea className="form-control mb-3 w-50 mx-auto" id="textarea" rows="3"></textarea>
-				</div>
-     			<input type='submit' className="btn btn-primary w-50 mx-auto" value="Add job" />
-     		</form>
-    	</div>
-    )
-  }
+	render() {
+		return (
+		 	<div className="container">
+		 		<h3 className="text-center mb-5">Add Job</h3>
+		 		<form onSubmit={(event) => this._onFormSubmit(event)} className="input-group d-flex flex-column">
+		 			<input className="form-control mb-3 w-50 mx-auto" type="text" placeholder="Title" />
+		 			<input className="form-control mb-3 w-50 mx-auto" type="text" placeholder="Company" />
+		 			<input className="form-control mb-3 w-50 mx-auto" type="text" placeholder="Location" />
+		 			<div className="form-group">
+						<label htmlFor="textarea" className="d-block text-center mx-auto">Description</label>
+						<textarea className="form-control mb-3 w-50 mx-auto" id="textarea" rows="3"></textarea>
+					</div>
+		 			<button type='submit' className="btn btn-primary w-50 mx-auto">Add Job</button>
+		 		</form>
+			</div>
+		)
+	}
 }
