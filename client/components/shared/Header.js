3b641a5911e5d9bcab4032eb55c55f4926004c52
codypearce <codyapearce@gmail.com>
2017-12-14 20:30:27 -0500

    Add find a job and post a job buttons, update header

--- client/components/shared/Header.js
+++ client/components/shared/Header.js
@@ -4,7 +4,14 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <h1 className="text-center my-2">Job Postings</h1>
+        <div className="mt-4">
+            <h1 className="text-center my-2">Job Postings</h1>
+            <nav className="d-flex justify-content-center mt-4">
+                <a href="#" className="btn btn-primary">Find a Job</a>
+                <a href="#" className="btn btn-secondary ml-1">Add a Job</a>
+            </nav>
+        </div>
+
     )
   }
 }
