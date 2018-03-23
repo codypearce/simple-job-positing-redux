abed36de3831ffd2273c73259e6c4a0ce99f349e
codypearce <outis161@gmail.com>
2018-03-22 20:20:22 -0700

    move active job page to own component

---
+++ client/components/pages/ActiveJobPage.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+export default class ActiveJob extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    _renderJob() {
+        const { job } = this.props;
+        return (
+            <div className="container">
+                <div className="backBtn">
+                    <button className="btn">Back to jobs</button>
+                </div>
+                <div className="text-center my-4">
+                    <h2 className="pointer">{job.title}</h2>
+                    <p>{job.company}</p>
+                </div>
+                <div className="my-4">
+                    <p>{job.description}</p>
+                </div>
+                <div className="my-4">
+                    <p>Compensation: {job.salary}</p>
+                    <p>Location: {job.location}</p>
+                    <div className="text-center">
+                        <button className="btn btn-primary">Apply</button>
+                        <button className="btn btn-warn">Not Interested</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    render() {
+        return (
+            <div>
+                {this.props.job ? (
+                    this._renderJob()
+                ) : (
+                    <div>No job Selected</div>
+                )}
+            </div>
+        );
+    }
+}
--- client/containers/ActiveJob.js
+++ client/containers/ActiveJob.js
@@ -1,49 +1,18 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import ActiveJobPage from "../components/pages/ActiveJobPage";
 
 class ActiveJob extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  _renderJob() {
-    const { job } = this.props;
-    return (
-      <div className="container">
-        <div className="backBtn">
-          <button className="btn">Back to jobs</button>
-        </div>
-        <div className="text-center my-4">
-            <h2 className="pointer">{job.title}</h2>
-            <p>{job.company}</p>
-        </div>
-        <div className="my-4">
-          <p>{job.description}</p>
-        </div>
-        <div className="my-4">
-          <p>Compensation: {job.salary}</p>
-          <p>Location: {job.location}</p>
-          <div className="text-center">
-            <button className="btn btn-primary">Apply</button>
-            <button className="btn btn-warn">Not Interested</button>
-          </div>
-        </div>
-      </div>
-      )
-  }
-  render() {
-    return (
-      <div>
-      {this.props.job ? this._renderJob() : <div>No job Selected</div>}
-    </div>
-    )
-  }
+    render() {
+        return <ActiveJobPage />;
+    }
 }
 
 function mapStateToProps(state) {
-  return {
-    job: state.activeJob,
-  };
+    return {
+        job: state.activeJob
+    };
 }
 
 export default connect(mapStateToProps)(ActiveJob);
