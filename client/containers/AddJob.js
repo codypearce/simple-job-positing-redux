36e15d5ec19f0a378be5a21d0c767cf7f5c46c44
codypearce <outis161@gmail.com>
2018-03-22 20:18:30 -0700

    move render logic for addjob page to own component

--- client/containers/AddJob.js
+++ client/containers/AddJob.js
@@ -1,58 +1,8 @@
 import React, { Component } from "react";
+import AddJobPage from "../components/pages/AddJobPage";
 
-export default class ActiveJob extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    _onFormSubmit(event) {
-        event.preventDefault();
-    }
-
+export default class AddJob extends Component {
     render() {
-        return (
-            <div className="container">
-                <h3 className="text-center mb-5">Add Job</h3>
-                <form
-                    onSubmit={event => this._onFormSubmit(event)}
-                    className="input-group d-flex flex-column"
-                >
-                    <input
-                        className="form-control mb-3 w-50 mx-auto"
-                        type="text"
-                        placeholder="Title"
-                    />
-                    <input
-                        className="form-control mb-3 w-50 mx-auto"
-                        type="text"
-                        placeholder="Company"
-                    />
-                    <input
-                        className="form-control mb-3 w-50 mx-auto"
-                        type="text"
-                        placeholder="Location"
-                    />
-                    <div className="form-group">
-                        <label
-                            htmlFor="textarea"
-                            className="d-block text-center mx-auto"
-                        >
-                            Description
-                        </label>
-                        <textarea
-                            className="form-control mb-3 w-50 mx-auto"
-                            id="textarea"
-                            rows="3"
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="btn btn-primary w-50 mx-auto"
-                    >
-                        Add Job
-                    </button>
-                </form>
-            </div>
-        );
+        return <AddJobPage />;
     }
 }
