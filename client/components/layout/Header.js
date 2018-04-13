812787679fee4c4e298ad5d6fd14da058598b77a
codypearce <outis161@gmail.com>
2018-04-12 21:26:20 -0700

    add home route and fix overflow

--- client/components/layout/Header.js
+++ client/components/layout/Header.js
@@ -4,8 +4,11 @@ import { Link } from "react-router-dom";
 export default class Header extends Component {
     render() {
         return (
-            <header className="d-flex justify-content-end">
-                <Link to="/login" className="btn">
+            <header className="d-flex align-items-center justify-content-end">
+                <Link to="/" className="btn">
+                    Home
+                </Link>
+                <Link to="/login" className="btn ml-3 mr-3">
                     Login
                 </Link>
             </header>
