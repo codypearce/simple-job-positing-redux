0c49f16f637acff3e4e0960ea9e83d72bf7bacd6
codypearce <outis161@gmail.com>
2018-04-13 18:09:44 -0700

    move header, body, and footer to own functions

--- client/components/ui/LoginModal.js
+++ client/components/ui/LoginModal.js
@@ -2,6 +2,63 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export default class LoginModal extends Component {
+    _renderHeader() {
+        return (
+            <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                    Login
+                </h5>
+                <button
+                    type="button"
+                    className="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                >
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        );
+    }
+    _renderBody() {
+        return (
+            <div className="modal-body">
+                <div className="form-group">
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                        placeholder="Email"
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="password"
+                        placeholder="Password"
+                    />
+                </div>
+            </div>
+        );
+    }
+    _renderFooter() {
+        return (
+            <div className="modal-footer">
+                <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-dismiss="modal"
+                >
+                    Cancel
+                </button>
+                <button type="button" className="btn btn-primary">
+                    Login
+                </button>
+            </div>
+        );
+    }
     render() {
         return (
             <div>
@@ -24,57 +81,9 @@ export default class LoginModal extends Component {
                 >
                     <div className="modal-dialog" role="document">
                         <div className="modal-content">
-                            <div className="modal-header">
-                                <h5
-                                    className="modal-title"
-                                    id="exampleModalLabel"
-                                >
-                                    Login
-                                </h5>
-                                <button
-                                    type="button"
-                                    className="close"
-                                    data-dismiss="modal"
-                                    aria-label="Close"
-                                >
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <div className="form-group">
-                                    <input
-                                        type="email"
-                                        className="form-control"
-                                        id="exampleInputEmail1"
-                                        aria-describedby="emailHelp"
-                                        placeholder="Email"
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        id="exampleInputEmail1"
-                                        aria-describedby="password"
-                                        placeholder="Password"
-                                    />
-                                </div>
-                            </div>
-                            <div className="modal-footer">
-                                <button
-                                    type="button"
-                                    className="btn btn-secondary"
-                                    data-dismiss="modal"
-                                >
-                                    Cancel
-                                </button>
-                                <button
-                                    type="button"
-                                    className="btn btn-primary"
-                                >
-                                    Login
-                                </button>
-                            </div>
+                            {this._renderHeader()}
+                            {this._renderBody()}
+                            {this._renderFooter()}
                         </div>
                     </div>
                 </div>
