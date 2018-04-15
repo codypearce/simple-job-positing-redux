824fa0e32da3a2add5ea21dbfa85480785b8fd79
codypearce <outis161@gmail.com>
2018-04-14 18:29:51 -0700

    set up logged in and logged out states in header

--- client/components/layout/Header.js
+++ client/components/layout/Header.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 import LoginModal from "../ui/LoginModal";
 import SignupModal from "../ui/SignupModal";
 
 export default class Header extends Component {
+    static propTypes = {
+        isLoggedIn: PropTypes.bool
+    };
+    _renderLoggedOut() {
+        return (
+            <span className="d-flex align-items-center justify-content-end">
+                <LoginModal />
+                <SignupModal />
+            </span>
+        );
+    }
+    _renderLoggedIn() {
+        return (
+            <Link to="/" className="btn">
+                Log Out
+            </Link>
+        );
+    }
     render() {
+        const { isLoggedIn } = this.props;
         return (
             <header className="d-flex align-items-center justify-content-end">
                 <Link to="/" className="btn">
                     Home
                 </Link>
-                <LoginModal />
-                <SignupModal />
+                {isLoggedIn ? this._renderLoggedIn() : this._renderLoggedOut()}
             </header>
         );
     }
