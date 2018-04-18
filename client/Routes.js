b777ede5026a190c5c1d3595a0c3664b3450b977
codypearce <outis161@gmail.com>
2018-04-17 20:32:38 -0700

    implement simple check if authenticated and force to homepage if not

--- client/Routes.js
+++ client/Routes.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
+import requireAuthentication from "./requireAuthentication";
+
 import JobIndex from "./containers/JobIndex";
 import JobDetail from "./containers/JobDetail";
 import JobCreate from "./containers/JobCreate";
@@ -10,7 +12,11 @@ export default class Routes extends Component {
         return (
             <Switch>
                 <Route exact path="/" component={JobIndex} />
-                <Route exact path="/jobs/create/" component={JobCreate} />
+                <Route
+                    exact
+                    path="/jobs/create/"
+                    component={requireAuthentication(JobCreate)}
+                />
                 <Route exact path="/jobs/:id" component={JobDetail} />
             </Switch>
         );
--- client/requireAuthentication.js
+++ client/requireAuthentication.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
 export default function(ComposedComponent) {
     class Authentication extends Component {
+        static propTypes = {
+            isLoggedIn: PropTypes.bool,
+            history: PropTypes.object
+        };
+        componentWillMount() {
+            if (!this.props.isLoggedIn) {
+                this.props.history.push("/");
+            }
+        }
+        componentWillReceiveProps(nextProps) {
+            console.log("test");
+            if (!nextProps.isLoggedIn) {
+                this.props.history.push("/");
+            } else {
+                console.log("you can go there!");
+            }
+        }
         render() {
             return <ComposedComponent {...this.props} />;
         }
     }
-    return Authentication;
+    function mapStateToProps(state) {
+        return { isLoggedIn: state.auth.isLoggedIn };
+    }
+    return withRouter(connect(mapStateToProps)(Authentication));
 }
